bd090939ca0a72500c57f830278ca5d9357b47e6
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2024-11-11 20:02:42 -0500

    Add more description to logger middleware

--- backend/api/middlewares/logger_middleware.py
+++ backend/api/middlewares/logger_middleware.py
@@ -4,17 +4,17 @@ from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 
 
-# TODO:(Member) Implement this logging middleware
 class LoggerMiddleware(BaseHTTPMiddleware):
     async def dispatch(
         self, request: Request, call_next: Callable[[Request], Any]
     ) -> Response:
         """
         Logs all incoming and outgoing request, response pairs. This method logs the request params,
-        datetime of request, length of time of request and response.
+        datetime of request, duration of execution. Logs should be printed using the custom logging module provided.
+        Logs should be printed so that they are easily readable and understandable.
 
-        @param request: Request received to this middleware
-        @param call_next: Endpoint or next middleware to be called
+        @param request: Request received to this middleware from client (it is supplied by FastAPI)
+        @param call_next: Endpoint or next middleware to be called (if any, this is the next middleware in the chain of middlewares, it is supplied by FastAPI)
         @return Response from endpoint
         """
         # TODO:(Member) Finish implementing this method
