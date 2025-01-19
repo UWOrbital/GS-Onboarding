92642c14aa31811109827fd869ac1353689fa903
James <66389450+Jam-Cai@users.noreply.github.com>
2025-01-19 14:43:02 -0500

    fixed frontend

--- frontend/src/input/command_input.tsx
+++ frontend/src/input/command_input.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./command_input.css"
 const CommandInput = () => {
   // TODO: (Member) Setup state and useEffect calls here
-  const { command, setCommand } = useState("");
+  const [command, setCommand] = useState("");
 
   const handleSubmit = () => {
     // TODO:(Member) Submit to your post endpoint 
