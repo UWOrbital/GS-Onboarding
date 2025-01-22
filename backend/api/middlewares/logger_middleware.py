88bc83ff92538bb78226a96a47f9e659965aa506
wqba <wabarurse@gmail.com>
2025-01-22 10:55:16 -0500

    fixed more changes in PR

--- backend/api/middlewares/logger_middleware.py
+++ backend/api/middlewares/logger_middleware.py
@@ -7,9 +7,7 @@ from ...utils.logging import logger
 
 
 class LoggerMiddleware(BaseHTTPMiddleware):
-    async def dispatch(
-        self, request: Request, call_next: Callable[[Request], Any]
-    ) -> Response:
+    async def dispatch(self, request: Request, call_next: Callable[[Request], Any]) -> Response:
         """
         Logs all incoming and outgoing request, response pairs. This method logs the request params,
         datetime of request, duration of execution. Logs should be printed using the custom logging module provided.
@@ -20,29 +18,33 @@ class LoggerMiddleware(BaseHTTPMiddleware):
         @return Response from endpoint
         """
 
-        #logger_setup() assuming this is run during startup
+        start_time = time()
 
         logger.info(f"Incoming request: {request.method} {request.url.path}");
         logger.info(f"Params: {request.query_params}");
         logger.info(f"Headers: {dict(request.headers)}");
-
-        start_time = time()
+    
+        try:
+            reqBody = await request.body()
+            logger.info(f"Request body: {reqBody}")
+        except Exception as e:
+            logger.warning(f"Error reading body!. Error: {str(e)}")
+        
 
         try:
             response = await call_next(request)
+            logger.info(f"Outgoing response: {request.method} {request.url.path}")
+            logger.info(f"Response status: {response.status_code}")
+            logger.info(f"Response headers: {dict(response.headers)}")
+
+            try:
+                resBody = await response.body()
+                logger.info(f"Response body: {resBody}")
+            except Exception as e:
+                logger.warning(f"Error reading body!. Error: {str(e)}")
+
         except Exception as e:
             logger.warning(f"Error calling next middleware!. Error: {str(e)}")
-
-
-        logger.info(f"Outgoing response: {request.method} {request.url.path}")
-        logger.info(f"Response status: {response.status_code}")
-        logger.info(f"Response headers: {dict(response.headers)}")
-        
-        try:
-            body = await request.json()
-            logger.info(f"Request body: {body}")
-        except Exception as e:
-            logger.warning(f"Error reading body!. Error: {str(e)}")
 
 
         end_time = time() - start_time
