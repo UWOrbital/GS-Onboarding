2fa2963f3d63e2774aaf9e083dd7fe735b13c9ca
Jiatao Geng <jiataogeng7@gmail.com>
2025-01-25 21:09:39 -0500

    fix create command

--- frontend/src/input/command_input.tsx
+++ frontend/src/input/command_input.tsx
@@ -6,36 +6,85 @@ import { createCommand, getMainCommands } from "./input_api"
 
 const CommandInput = () => {
   // TODO: (Member) Setup state and useEffect calls here
-  const [commandType, setCommandType] = useState<string>("1")
-  const [params, setParams] = useState<string>("")
   const [mainCommands, setMainCommands] = useState<MainCommandResponse[]>([])
-
+  const [commandType, setCommandType] = useState<MainCommandResponse | null>(null)
+  const [params, setParams] = useState<{[key: string] : string}>({})
   
   useEffect(() => {
     const setMainCommandsFn = async () => {
       const data = await getMainCommands()
       setMainCommands(data.data)
-    }
 
+      setCommandType(data.data[0])
+
+      const parameters = commandType?.params?.split(",") || []
+      let paramsObject : { [key: string]: string } = {}
+      for(const param of parameters) {
+        paramsObject[param] = ""
+      }
+      setParams(paramsObject);
+    }
     setMainCommandsFn();
   }, [])
   
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    // TODO:(Member) Submit to your post endpoint
+    // TODO:(Member) Submit to your post endpoint    
     e.preventDefault();
+
+    //check if parameters are valid
+    const allParams = commandType?.params?.split(",") || [];
+    const paramsList = []
+    for (const param of allParams) {
+      if (!params[param]) {
+        alert("Parameter missing")
+        return;
+      }
+      paramsList.push(params[param])
+    }
+    
+
     const reqBody = {
-      command_type: +commandType,
-      params: params,
+      command_type: commandType?.id || 0,
+      params: paramsList.join(",")
     };
 
     //console.log('Req body:', reqBody);
 
+    
     const createCommandFn = async () => {
       await createCommand(reqBody);
+      window.location.reload();
     }
-
     createCommandFn();
+  }
+
+  /*
+          <input value={params} onChange={(e) => setParams(e.target.value)}/>
+          {// Use parameters of command type to output input boxes}
+          { .params.map(param => (<input id={} value={}>{cmd.name}</option>))}
+  */
+  //console.log(`Command: `, commandType)
+  //console.log(`Parameters: `, params)
+
+  const changeCommandType = (e: any) => {
+    setCommandType(mainCommands.find(cmd => cmd.id == +e.target.value) || null)
+    const parameters = commandType?.params?.split(",") || []
+    let paramsObject : { [key: string]: string } = {}
+    for(const param of parameters) {
+      paramsObject[param] = ""
+    }
+    setParams(paramsObject);
+  }
+
+  const changeParam = (e: any) => {
+    const param = e.target.id
+    const value = e.target.value
+
+    setParams(prevParams => ({
+      ...prevParams,
+      [param]: value,
+    }));
   }
 
   return (
@@ -44,15 +93,21 @@ const CommandInput = () => {
         <div className="spreader">
           <div>
             <label>Command Type: </label>
-            <select value={commandType} onChange={(e) => setCommandType(e.target.value)}>{/* TODO: (Member) Display the list of commands based on the get commands request*/}
+            {/* TODO: (Member) Display the list of commands based on the get commands request*/}
+            <select 
+              value={commandType ? commandType.id : ""} 
+              onChange={changeCommandType}>
               {mainCommands.map(cmd => (<option value={cmd.id}>{cmd.name}</option>))}
             </select>
           </div>
-          <input value={params} onChange={(e) => setParams(e.target.value)}/> {/* TODO: (Member) Add input handling here if the selected command has a param input*/}
+        
+           {/* TODO: (Member) Add input handling here if the selected command has a param input*/}
+          {commandType && 
+            commandType.params?.split(",").map((param) => (<input id={param} placeholder={`Enter ${param}`} onChange={changeParam}/>))}
           <button type="submit">Submit</button>
         </div>
       </form>
-    </>
+    </> 
   )
 }
 
