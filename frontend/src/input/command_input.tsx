2822edb31ade53655a6f41cadcc20c9807996e94
Jiatao Geng <jiataogeng7@gmail.com>
2025-02-02 18:49:52 -0500

    complete new changes for frontend

--- frontend/src/input/command_input.tsx
+++ frontend/src/input/command_input.tsx
@@ -7,40 +7,43 @@ import { createCommand, getMainCommands } from "./input_api"
 const CommandInput = () => {
   // TODO: (Member) Setup state and useEffect calls here
   const [mainCommands, setMainCommands] = useState<MainCommandResponse[]>([])
-  const [commandType, setCommandType] = useState<MainCommandResponse | null>(null)
-  const [params, setParams] = useState<{[key: string] : string}>({})
+  const [commandType, setCommandType] = useState<MainCommandResponse>({id: 0, name: "", params: null, format: null, data_size: 0, total_size: 0})
+  const [params, setParams] = useState<Map<string, string>>(new Map());
   
   useEffect(() => {
     const setMainCommandsFn = async () => {
       const data = await getMainCommands()
+      if(!data.data) {
+        alert("Error occured. Please try again later.")
+      }
       setMainCommands(data.data)
-
       setCommandType(data.data[0])
 
-      const parameters = commandType?.params?.split(",") || []
-      let paramsObject : { [key: string]: string } = {}
+      const parameters = data.data[0].params?.split(",") || []
+      let paramsObject : Map<string, string> = new Map()
+    
       for(const param of parameters) {
-        paramsObject[param] = ""
+        paramsObject.set(param, "")
       }
       setParams(paramsObject);
     }
     setMainCommandsFn();
   }, [])
-  
+
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     // TODO:(Member) Submit to your post endpoint    
     e.preventDefault();
 
-    //check if parameters are valid
+    //Check if parameters are valid
     const allParams = commandType?.params?.split(",") || [];
     const paramsList = []
     for (const param of allParams) {
-      if (!params[param]) {
+      if (!params.get(param)) {
         alert("Parameter missing")
         return;
       }
-      paramsList.push(params[param])
+      paramsList.push(params.get(param))
     }
     
 
@@ -48,8 +51,6 @@ const CommandInput = () => {
       command_type: commandType?.id || 0,
       params: paramsList.join(",")
     };
-
-    //console.log('Req body:', reqBody);
 
     
     const createCommandFn = async () => {
@@ -59,32 +60,26 @@ const CommandInput = () => {
     createCommandFn();
   }
 
-  /*
-          <input value={params} onChange={(e) => setParams(e.target.value)}/>
-          {// Use parameters of command type to output input boxes}
-          { .params.map(param => (<input id={} value={}>{cmd.name}</option>))}
-  */
-  //console.log(`Command: `, commandType)
-  //console.log(`Parameters: `, params)
-
-  const changeCommandType = (e: any) => {
-    setCommandType(mainCommands.find(cmd => cmd.id == +e.target.value) || null)
-    const parameters = commandType?.params?.split(",") || []
-    let paramsObject : { [key: string]: string } = {}
+  const changeCommandType = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const type : MainCommandResponse = mainCommands.find(cmd => cmd.id == +e.target.value) || {id: 0, name: "", params: null, format: null, data_size: 0, total_size: 0}
+    setCommandType(type)
+    const parameters = type?.params?.split(",") || []
+    let paramsObject : Map<string, string> = new Map()
     for(const param of parameters) {
-      paramsObject[param] = ""
+      paramsObject.set(param, "")
     }
-    setParams(paramsObject);
+    setParams(paramsObject)
   }
 
-  const changeParam = (e: any) => {
+  const changeParam = (e: React.ChangeEvent<HTMLInputElement>) => {
     const param = e.target.id
     const value = e.target.value
 
-    setParams(prevParams => ({
-      ...prevParams,
-      [param]: value,
-    }));
+    setParams(prevParams => {
+      const newParams = new Map(prevParams);
+      newParams.set(param, value);
+      return newParams;
+    })
   }
 
   return (
@@ -95,7 +90,7 @@ const CommandInput = () => {
             <label>Command Type: </label>
             {/* TODO: (Member) Display the list of commands based on the get commands request*/}
             <select 
-              value={commandType ? commandType.id : ""} 
+              value={commandType.id} 
               onChange={changeCommandType}>
               {mainCommands.map(cmd => (<option value={cmd.id}>{cmd.name}</option>))}
             </select>
@@ -103,7 +98,8 @@ const CommandInput = () => {
         
            {/* TODO: (Member) Add input handling here if the selected command has a param input*/}
           {commandType && 
-            commandType.params?.split(",").map((param) => (<input id={param} placeholder={`Enter ${param}`} onChange={changeParam}/>))}
+            commandType.params?.split(",").map((param) => (<input value={params.get(param)} id={param} placeholder={`Enter ${param}`} onChange={changeParam}/>))}
+            
           <button type="submit">Submit</button>
         </div>
       </form>
