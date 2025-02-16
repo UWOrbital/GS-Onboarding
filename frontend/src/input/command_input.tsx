cce41712b27d469bde543228d98b9b07bd93cf55
Suhas Unni <suhasunni6@gmail.com>
2025-02-15 21:47:44 -0500

    revert command_input

--- frontend/src/input/command_input.tsx
+++ frontend/src/input/command_input.tsx
@@ -1,10 +1,17 @@
+import React, { ReactElement, useEffect, useState } from "react"
 import "./command_input.css"
+import { MainCommandListResponse, MainCommandResponse } from "../data/response"
+import { createCommand, getMainCommands } from "./input_api"
+import { CommandRequest } from "../data/request"
+
 
 const CommandInput = () => {
   // TODO: (Member) Setup state and useEffect calls here
-
-  const handleSubmit = () => {
-    // TODO:(Member) Submit to your post endpoint 
+  
+  
+  const handleSubmit = async (e:React.FormEvent) => {
+    // TODO:(Member) Submit to your post endpoint
+     
   }
 
   return (
@@ -13,13 +20,13 @@ const CommandInput = () => {
         <div className="spreader">
           <div>
             <label>Command Type: </label>
-            <select>{/* TODO: (Member) Display the list of commands based on the get commands request*/}
+            <select>
               <option value={"1"}>Command 1</option>
               <option value={"2"}>Command 2</option>
-              <option value={"3"}>Command 3</option>
+              <option value={"3"}>Command3</option>
             </select>
           </div>
-          <input /> {/* TODO: (Member) Add input handling here if the selected command has a param input*/}
+          <input/> {/* TODO: (Member) Add input handling here if the selected command has a param input*/}
           <button onClick={handleSubmit}>Submit</button>
         </div>
       </form>
