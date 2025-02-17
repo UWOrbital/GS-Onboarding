80cd261a01dca2132101d4e976e2d4f5e6aee604
Suhas Unni <suhasunni6@gmail.com>
2025-02-17 11:03:11 -0500

    "implement requested changes"

--- frontend/src/display/table.tsx
+++ frontend/src/display/table.tsx
@@ -14,7 +14,6 @@ const CommandTable = () => {
 
     getCommandsFn();
   }, [])
-
   const handleDelete = (id: number) => {
     return async () => {
       // TODO: (Member) Handle delete logic here
