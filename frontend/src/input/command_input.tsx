cc1fb77881ecf0be46a501556429b44c3e876216
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2025-07-16 15:23:51 -0400

    make frontend easier

--- frontend/src/input/command_input.tsx
+++ frontend/src/input/command_input.tsx
@@ -1,4 +1,5 @@
-import { CommandResponse } from "../data/response"
+import { useState } from "react";
+import { CommandResponse, MainCommandResponse } from "../data/response"
 import "./command_input.css"
 
 interface CommandInputProp {
@@ -6,26 +7,49 @@ interface CommandInputProp {
 }
 
 const CommandInput = ({ setCommands }: CommandInputProp) => {
-  // TODO: (Member) Setup state and useEffect calls here
+  const [selectedCommand, setSelectedCommand] = useState<MainCommandResponse | null>(null);
+  const [parameters, setParameters] = useState<{ [key: string]: string }>({});
+  // TODO: (Member) Setup anymore states if necessary
 
-  const handleSubmit = () => {
+  // TODO: (Member) Fetch MainCommands in a useEffect
+
+  const handleParameterChange = (param: string, value: string): void => {
+    setParameters((prev) => ({
+      ...prev,
+      [param]: value,
+    }));
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
     // TODO:(Member) Submit to your post endpoint 
   }
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="spreader">
           <div>
             <label>Command Type: </label>
-            <select>{/* TODO: (Member) Display the list of commands based on the get commands request*/}
+            <select>{/* TODO: (Member) Display the list of commands based on the get commands request.
+                        It should update the `selectedCommand` field when selecting one.*/}
               <option value={"1"}>Command 1</option>
               <option value={"2"}>Command 2</option>
               <option value={"3"}>Command 3</option>
             </select>
           </div>
-          <input /> {/* TODO: (Member) Add input handling here if the selected command has a param input*/}
-          <button onClick={handleSubmit}>Submit</button>
+          {selectedCommand?.params?.split(",").map((param) => (
+            <div key={param}>
+              <label htmlFor={`param-${param}`}>{param}: </label>
+              <input
+                id={`param-${param}`}
+                type="text"
+                value={parameters[param] || ""}
+                onChange={(e) => handleParameterChange(param, e.target.value)}
+                placeholder={`Enter ${param}`}
+              />
+            </div>
+          ))}
+          <button type="submit">Submit</button>
         </div>
       </form>
     </>
