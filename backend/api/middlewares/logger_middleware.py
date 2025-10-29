99e66e640e5362a84a89dadc0c7bd9ecd5e7bb09
alexli8408 <alexli8408@gmail.com>
2025-10-29 12:35:18 -0400

    Changes

--- backend/api/middlewares/logger_middleware.py
+++ backend/api/middlewares/logger_middleware.py
@@ -22,9 +22,7 @@ class LoggerMiddleware(BaseHTTPMiddleware): # Middleware
         # TODO:(Member) Finish implementing this method
         query = f"?{request.url.query}" if request.url.query else "" # True if request.url.query is non-empty
         start = perf_counter()
-        try:
-            response = await(call_next(request))
-            logger.info(f"{request.method} {request.url.path}{query} -> {response.status_code} in {(perf_counter() - start) * 1000:.2f}ms")
-        except Exception: # Catches every error
-            logger.exception(f"{request.method} {request.url.path}{query} -> error after {(perf_counter() - start) * 1000:.2f}ms") # Logs the exception from Exception
-            raise # Return value
+        response = await(call_next(request))
+        elapsed_ms = (perf_counter - start) * 1000
+        logger.info(f"{request.method} {request.url.path}{query} -> {response.status_code} in {elapsed_ms:.2f}ms")
+        return response
