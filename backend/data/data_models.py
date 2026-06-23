4fdca52f96f589ec8eab2f351eafd6ee4fb3ab07
pinwheeeel <152241032+pinwheeeel@users.noreply.github.com>
2026-06-23 13:43:09 -0400

    implement validation for MainCommand model

--- backend/data/data_models.py
+++ backend/data/data_models.py
@@ -34,8 +34,19 @@ class MainCommand(BaseSQLModel, table=True):
         In either of these cases return self. Otherwise raise a ValueError.
         The format of the comma seperated values is "data1,data2" so no spaces between data and the commas.
         """
-        # TODO: (Member) Implement this method
-        return self
+        if any(
+            [
+                self.params is None and self.format is None,
+                self.params is not None
+                and self.format is not None
+                and len(self.params.split(",")) == len(self.format.split(",")),
+            ]
+        ):
+            return self
+
+        raise ValueError(
+            "params and format must both be None, or have the same number of comma separated values"
+        )
 
 
 class Command(BaseSQLModel, table=True):
