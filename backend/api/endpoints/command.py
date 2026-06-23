f51e50df2fad7415d57afa0738e50e683fb469b1
pinwheeeel <152241032+pinwheeeel@users.noreply.github.com>
2026-06-23 13:43:46 -0400

    implement post/delete endpoints for Command

--- backend/api/endpoints/command.py
+++ backend/api/endpoints/command.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 
 from backend.api.models.request_model import CommandRequest
@@ -25,22 +25,42 @@ def get_commands(
 
 
 @command_router.post("/", response_model=CommandSingleResponse)
-def create_command(payload: CommandRequest) -> CommandSingleResponse:
+def create_command(
+    payload: CommandRequest,
+    db: Session = Depends(get_db),  # noqa: B008
+) -> CommandSingleResponse:
     """
     Creates an item with the given payload in the database and returns this payload after pulling it from the database
 
     :param payload: The data used to create an item
     :return: returns a json object with field of "data" under which there is the payload now pulled from the database
     """
-    # TODO:(Member) Implement this endpoint
+    command = Command(**payload.model_dump())
+
+    db.add(command)
+    db.commit()
+    db.refresh(command)
+
+    return {"data": command}
 
 
 @command_router.delete("/{id}", response_model=CommandListResponse)
-def delete_command(id: int) -> CommandSingleResponse:
+def delete_command(
+    id: int,
+    db: Session = Depends(get_db),  # noqa: B008
+) -> CommandSingleResponse:
     """
     Deletes the item with the given id if it exists. Otherwise raises a 404 error.
 
     :param id: The id of the item to delete
     :return: returns the list of commands after deleting the item
     """
-    # TODO:(Member) Implement this endpoint
+    command = db.get(Command, id)
+
+    if command is None:
+        raise HTTPException(status_code=404, detail="Command not found")
+
+    db.delete(command)
+    db.commit()
+
+    return get_commands(db)
